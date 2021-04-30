3b0cdc595a4c6382b2a595ade180c49e61e9d99e
naluechai <film6090@gmail.com>
2021-04-30 15:50:20 +0700

    add index template & migration pudate error

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=100)
+    pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
